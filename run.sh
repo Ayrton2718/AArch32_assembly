1de90d0befaee5d6e29ab742fdecbd327afd09c5
Ayrton2718 <matsushita.sena@gmail.com>
2022-04-24 09:02:23 +0000

    First commit

---
+++ run.sh
@@ -0,0 +1,8 @@
+set -e
+
+arm-linux-gnueabihf-gcc -g -mcpu=arm7 -mfpu=fpv4-sp-d16 -mfloat-abi=hard -o hello32.elf hello32.c --static
+qemu-arm -L /usr/arm-linux-gnueabihf -g 1234 ./hello32.elf
+
+
+# arm-none-eabi-gcc -g -mcpu=cortex-m4 -MMD -MP -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -ffunction-sections -fdata-sections -fstack-usage -std=gnu11 -o hello32.elf hello32.c --specs=nosys.specs
+# arm-none-eabi-gcc -g -mcpu=cortex-m4 -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -std=gnu11 -o hello32.elf hello32.c --specs=nosys.specs
